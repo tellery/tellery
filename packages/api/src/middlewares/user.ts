f5e5fe5e87caeaa710f67b20ce844d8ab08ffbde
domgoer <domdoumc@gmail.com>
2021-07-07 12:20:27 +0800

    update all dependences

--- packages/api/src/middlewares/user.ts
+++ packages/api/src/middlewares/user.ts
@@ -1,5 +1,5 @@
 import { Context, Next } from 'koa'
-import _, { isNull } from 'lodash'
+import _ from 'lodash'
 
 import userService from '../services/user'
 import { USER_TOKEN_HEADER_KEY } from '../utils/user'
@@ -11,7 +11,7 @@ export default async function user(ctx: Context, next: Next) {
   const token = ctx.headers[USER_TOKEN_HEADER_KEY] || ctx.cookies.get(USER_TOKEN_HEADER_KEY)
   let payload: { userId: string; expiresAt: number } | undefined
 
-  if (token) {
+  if (token && _.isString(token)) {
     payload = await userService.verifyToken(token)
     ctx.state.user = { id: payload.userId }
   }
@@ -26,7 +26,7 @@ export default async function user(ctx: Context, next: Next) {
   const { auth_token: at } = ctx
 
   // set token
-  if (at || isNull(at)) {
+  if (at || _.isNull(at)) {
     ctx.set(USER_TOKEN_HEADER_KEY, at ?? '')
     ctx.cookies.set(USER_TOKEN_HEADER_KEY, at, {
       expires: new Date(new Date().getTime() + 10 * 365 * 24 * 60 * 60), // never expires
