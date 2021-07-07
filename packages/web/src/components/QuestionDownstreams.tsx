98165d609bf9970d4ba0383cd4e1e7f4ff0c8acd
Found Pan Tiger <renzholy@gmail.com>
2021-07-07 15:41:47 +0800

    feat: QuestionDownstreams title

--- packages/web/src/components/QuestionDownstreams.tsx
+++ packages/web/src/components/QuestionDownstreams.tsx
@@ -1,4 +1,5 @@
 import { useOnScreen } from '@app/hooks'
+import { ThemingVariables } from '@app/styles'
 import type { Ref } from '@app/types'
 import { css, cx } from '@emotion/css'
 import { useQuestionDownstreams } from 'hooks/api'
@@ -6,7 +7,7 @@ import { groupBy, map } from 'lodash'
 import React, { useEffect, useRef, useMemo } from 'react'
 import { StoryRefs } from './StoryBackwardsRefs'
 
-export default function QuestionReferences(props: { blockId: string; className?: string }) {
+export default function QuestionDownstreams(props: { blockId: string; className?: string }) {
   const ref = useRef(null)
   const isOnScreen = useOnScreen(ref)
   const { data: items, refetch } = useQuestionDownstreams(props.blockId)
@@ -27,6 +28,15 @@ export default function QuestionReferences(props: { blockId: string; className?:
         props.className
       )}
     >
+      <h3
+        className={css`
+          margin: 0;
+          padding: 10px 20px;
+          color: ${ThemingVariables.colors.text[0]};
+        `}
+      >
+        Downstream{items.length > 1 ? 's' : ''} ({items.length})
+      </h3>
       {map(data, (blocks, storyId) => (
         <StoryRefs
           key={storyId}
