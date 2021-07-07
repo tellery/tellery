24df21a9a8d6be575ec522baaf0ee317a845c45f
Found Pan Tiger <renzholy@gmail.com>
2021-07-07 13:10:45 +0800

    feat: QuestionReferences use StoryRefs

--- packages/web/src/components/QuestionReferences.tsx
+++ packages/web/src/components/QuestionReferences.tsx
@@ -1,81 +1,39 @@
 import { useOnScreen } from '@app/hooks'
+import type { Ref } from '@app/types'
 import { css, cx } from '@emotion/css'
 import { useQuestionDownstreams } from 'hooks/api'
-import React, { useEffect, useRef } from 'react'
-import { ThemingVariables } from 'styles'
-import { BlockTitle } from './editor'
-import { useOpenQuestionBlockIdHandler } from './StoryQuestionsEditor'
+import { groupBy, map } from 'lodash'
+import React, { useEffect, useRef, useMemo } from 'react'
+import { StoryRefs } from './StoryBackwardsRefs'
 
 export default function QuestionReferences(props: { blockId: string; className?: string }) {
   const ref = useRef(null)
   const isOnScreen = useOnScreen(ref)
-  const { data: items, blocks, refetch } = useQuestionDownstreams(props.blockId)
-  const handleOpen = useOpenQuestionBlockIdHandler()
+  const { data: items, refetch } = useQuestionDownstreams(props.blockId)
   useEffect(() => {
     if (isOnScreen) {
       refetch()
     }
   }, [isOnScreen, refetch])
+  const data = useMemo(() => groupBy(items, 'storyId'), [items])
 
-  return items.length ? (
-    <ul
-      ref={ref}
+  return (
+    <div
       className={cx(
         css`
-          width: 300px;
-          overflow-y: auto;
-          list-style-type: none;
-          padding-inline-start: 0;
-          margin: 0;
-          & > li {
-            cursor: pointer;
-          }
+          padding: 5px;
+          overflow-y: scroll;
         `,
         props.className
       )}
     >
-      {items.map(({ id, storyId }) => (
-        <li
-          key={id}
-          className={css`
-            background: ${ThemingVariables.colors.primary[5]};
-            border-radius: 10px;
-            margin: 10px;
-            padding: 10px;
-            cursor: pointer;
-          `}
-          onClick={() => {
-            if (storyId) {
-              handleOpen({ mode: 'SQL', blockId: id, storyId, readonly: false })
-            }
-          }}
-        >
-          {blocks?.[id] ? (
-            <span
-              className={css`
-                font-weight: 500;
-                font-size: 14px;
-                line-height: 24px;
-                color: ${ThemingVariables.colors.text[0]};
-              `}
-            >
-              <BlockTitle block={blocks?.[id]}></BlockTitle>
-            </span>
-          ) : null}
-          {storyId && blocks?.[storyId] ? (
-            <span
-              className={css`
-                font-size: 14px;
-                line-height: 16px;
-                color: ${ThemingVariables.colors.text[1]};
-              `}
-            >
-              <br />
-              <BlockTitle block={blocks?.[storyId]}></BlockTitle>
-            </span>
-          ) : null}
-        </li>
+      {map(data, (blocks, storyId) => (
+        <StoryRefs
+          key={storyId}
+          storyId={storyId}
+          refs={blocks.filter(({ storyId }) => !!storyId).map(({ id, storyId }) => ({ blockId: id, storyId })) as Ref[]}
+        />
       ))}
-    </ul>
-  ) : null
+    </div>
+  )
 }
