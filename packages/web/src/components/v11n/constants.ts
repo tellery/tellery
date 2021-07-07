888f34edf623e918ef4d470c654882bbf184fcd1
Found Pan Tiger <renzholy@gmail.com>
2021-07-07 14:59:57 +0800

    chore: use system font

--- packages/web/src/components/v11n/constants.ts
+++ packages/web/src/components/v11n/constants.ts
@@ -1,3 +1,2 @@
-export const fontFamily = `Sarabun, -apple-system, 'PingFang SC', BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Segoe UI, Ubuntu,
-Roboto, Oxygen, Ubuntu, Cantarell, Microsoft YaHei UI, Microsoft YaHei, Source Han Sans CN, 'WenQuanYi Micro Hei',
-sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`
+export const fontFamily = `-apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, 'Apple Color Emoji', Arial, sans-serif,
+'Segoe UI Emoji', 'Segoe UI Symbol'`
