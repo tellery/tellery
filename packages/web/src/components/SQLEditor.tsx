5f2171f46f2f171bfd1b7992e7b04b387c4c1129
Found Pan Tiger <renzholy@gmail.com>
2021-07-13 14:38:44 +0800

    Update SQLEditor.tsx

--- packages/web/src/components/SQLEditor.tsx
+++ packages/web/src/components/SQLEditor.tsx
@@ -11,26 +11,9 @@ import MonacoEditor, { useMonaco } from '@monaco-editor/react'
 import { compact, uniq } from 'lodash'
 import type { editor } from 'monaco-editor/esm/vs/editor/editor.api'
 import { useCallback, useEffect, useMemo, useState } from 'react'
+import { createPortal } from 'react-dom'
 import { useGetBlockTitleTextSnapshot } from './editor'
 import { useQuestionEditor } from './StoryQuestionsEditor'
-
-const contentWidgetClassname = css`
-  font-size: 12px;
-  line-height: 18px;
-  vertical-align: middle;
-  border-radius: 6px;
-  padding: 0 5px 0 23px;
-  color: ${ThemingVariables.colors.text[0]};
-  background-color: ${ThemingVariables.colors.primary[4]};
-  background-image: ${SVG2DataURI(IconMenuQuery)};
-  background-size: 16px;
-  background-repeat: no-repeat;
-  background-position: 5px 50%;
-  white-space: nowrap;
-  cursor: pointer;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`
 
 const STORY_BLOCK_REGEX = new RegExp(`${window.location.protocol}//${window.location.host}/story/(\\S+)#(\\S+)`)
 
@@ -100,52 +83,34 @@ export function SQLEditor(props: {
     [props.padding]
   )
   const workspace = useWorkspace()
-  const [questionIds, setQuestionIds] = useState<string[]>([])
+  const [matches, setMatches] = useState<editor.FindMatch[]>([])
+  const questionIds = useMemo(() => uniq(compact(matches.map((match) => match.matches?.[1]))), [matches])
   const { data: questions } = useMgetBlocks(questionIds)
-  const getBlockTitle = useGetBlockTitleTextSnapshot()
-  const openStoryHandler = useOpenStory()
-  const { open } = useQuestionEditor()
-  const mapMatchToQuestionId = useCallback((match: editor.FindMatch) => match.matches?.[1], [])
-  const mapMatchToContentWidget = useCallback(
-    (match: editor.FindMatch, index: number) => {
-      const questionId = match.matches?.[1]
-      const block = (questionId ? questions?.[questionId] : undefined) as Editor.QuestionBlock | undefined
-      if (!block) {
-        return undefined
-      }
-      return {
-        getId: () => {
-          return `content.widget.transclusion.${questionId}.${index}`
-        },
-        getDomNode: () => {
-          const domNode = document.createElement('div')
-          domNode.innerHTML = getBlockTitle(block)
-          domNode.className = contentWidgetClassname
-          if (match.matches?.[0]) {
-            domNode.style.width = `${match.matches[0].length * 0.955}ch`
-          }
-          domNode.title = `${getBlockTitle(block)}\n${block.content?.sql || ''}`
-          domNode.onclick = () => {
-            if (!block.storyId) {
-              return
-            }
-            openStoryHandler(block.storyId, { blockId: block.id, isAltKeyPressed: true })
-            open({ mode: 'SQL', storyId: block.storyId, blockId: block.id, readonly: false })
-          }
-          return domNode
-        },
-        getPosition: () => {
-          return {
-            position: match.range.getStartPosition(),
-            range: match.range,
-            preference: [0]
-          }
+  const mapMatchToContentWidget = useCallback((match: editor.FindMatch, index: number) => {
+    const questionId = match.matches?.[1]
+    if (!questionId) {
+      return null
+    }
+    return {
+      getId: () => {
+        return `content.widget.transclusion.${questionId}.${index}`
+      },
+      getDomNode: () => {
+        return document.createElement('div')
+      },
+      getPosition: () => {
+        return {
+          position: match.range.getStartPosition(),
+          range: match.range,
+          preference: [0]
         }
       }
-    },
-    [getBlockTitle, open, openStoryHandler, questions]
+    }
+  }, [])
+  const contentWidgets = useMemo<editor.IContentWidget[]>(
+    () => compact(matches.map(mapMatchToContentWidget)),
+    [mapMatchToContentWidget, matches]
   )
-  const [contentWidgets, setContentWidgets] = useState<editor.IContentWidget[]>([])
   useEffect(() => {
     if (!editor) {
       return
@@ -154,17 +119,12 @@ export function SQLEditor(props: {
     if (!model) {
       return
     }
-    const matches = model.findMatches(transclusionRegex.source, true, true, true, null, true)
-    setQuestionIds(uniq(compact(matches.map(mapMatchToQuestionId))))
-    setContentWidgets(compact(matches.map(mapMatchToContentWidget)))
+    setMatches(model.findMatches(transclusionRegex.source, true, true, true, null, true))
     const { dispose } = model.onDidChangeContent(() => {
-      const matches = model.findMatches(transclusionRegex.source, true, true, true, null, true)
-      setQuestionIds(uniq(compact(matches.map(mapMatchToQuestionId))))
-      setContentWidgets(compact(matches.map(mapMatchToContentWidget)))
+      setMatches(model.findMatches(transclusionRegex.source, true, true, true, null, true))
     })
     return dispose
-  }, [editor, mapMatchToContentWidget, mapMatchToQuestionId, workspace.id])
-
+  }, [editor, workspace.id])
   useEffect(() => {
     if (!editor) {
       return
@@ -180,26 +140,85 @@ export function SQLEditor(props: {
   }, [editor, contentWidgets])
 
   return (
-    <MonacoEditor
-      className={cx(
-        css`
-          .scroll-decoration {
-            display: none;
-          }
-          .detected-link {
-            text-decoration: unset !important;
-          }
-        `
-      )}
-      wrapperClassName={props.className}
-      theme="tellery"
-      value={props.value}
-      onChange={handleChange}
-      height="100%"
-      width="100%"
-      language={props.languageId}
-      options={options}
-      onMount={setEditor}
-    />
+    <>
+      {matches.map((match, index) => {
+        if (!match.matches) {
+          return null
+        }
+        const questionId = match.matches[1]
+        return questions?.[questionId] ? (
+          <TransclusionContentWidget
+            key={questionId}
+            value={questions[questionId]}
+            length={match.matches[0].length}
+            index={index}
+          />
+        ) : null
+      })}
+      <MonacoEditor
+        className={cx(
+          css`
+            .scroll-decoration {
+              display: none;
+            }
+            .detected-link {
+              text-decoration: unset !important;
+            }
+          `
+        )}
+        wrapperClassName={props.className}
+        theme="tellery"
+        value={props.value}
+        onChange={handleChange}
+        height="100%"
+        width="100%"
+        language={props.languageId}
+        options={options}
+        onMount={setEditor}
+      />
+    </>
   )
+}
+
+function TransclusionContentWidget(props: { value: Editor.QuestionBlock; length: number; index: number }) {
+  const { value: block } = props
+  const getBlockTitle = useGetBlockTitleTextSnapshot()
+  const openStoryHandler = useOpenStory()
+  const { open } = useQuestionEditor()
+  const el = document.querySelector(`[widgetid="content.widget.transclusion.${block.id}.${props.index}"]`)
+
+  return el
+    ? createPortal(
+        <div
+          className={css`
+            font-size: 12px;
+            line-height: 18px;
+            vertical-align: middle;
+            border-radius: 6px;
+            padding: 0 5px 0 23px;
+            color: ${ThemingVariables.colors.text[0]};
+            background-color: ${ThemingVariables.colors.primary[4]};
+            background-image: ${SVG2DataURI(IconMenuQuery)};
+            background-size: 16px;
+            background-repeat: no-repeat;
+            background-position: 5px 50%;
+            white-space: nowrap;
+            cursor: pointer;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            width: ${props.length * 0.955}ch;
+          `}
+          onClick={() => {
+            if (!block.storyId) {
+              return
+            }
+            openStoryHandler(block.storyId, { blockId: block.id, isAltKeyPressed: true })
+            open({ mode: 'SQL', storyId: block.storyId, blockId: block.id, readonly: false })
+          }}
+        >
+          {getBlockTitle(block)}
+        </div>,
+        el
+      )
+    : null
 }
