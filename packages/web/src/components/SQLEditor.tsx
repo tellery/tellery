a163c4a76ec18d5bbe2c38bb678e614d99dcce6f
Found Pan Tiger <renzholy@gmail.com>
2021-08-12 17:35:38 +0800

    Update SQLEditor.tsx

--- packages/web/src/components/SQLEditor.tsx
+++ packages/web/src/components/SQLEditor.tsx
@@ -5,17 +5,18 @@ import { useMgetBlocks } from '@app/hooks/api'
 import { transclusionRegex } from '@app/hooks/useSqlEditor'
 import { SVG2DataURI } from '@app/lib/svg'
 import { ThemingVariables } from '@app/styles'
-import type { Editor } from '@app/types'
+import { Editor } from '@app/types'
 import { css, cx } from '@emotion/css'
 import MonacoEditor, { useMonaco } from '@monaco-editor/react'
 import Tippy from '@tippyjs/react'
-import { compact, uniq } from 'lodash'
+import { compact, uniq, omit } from 'lodash'
 import type { editor } from 'monaco-editor/esm/vs/editor/editor.api'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { useGetBlockTitleTextSnapshot } from './editor'
 import { useQuestionEditor } from './StoryQuestionsEditor'
 import { SQLViewer } from './SQLViewer'
+import YAML from 'yaml'
 
 const STORY_BLOCK_REGEX = new RegExp(`${window.location.protocol}//${window.location.host}/story/(\\S+)#(\\S+)`)
 
@@ -224,6 +225,8 @@ function TransclusionContentWidget(props: {
   )
   const title = getBlockTitle(block)
   const [visible, setVisible] = useState(false)
+  const isDBT = block.type === Editor.BlockType.DBT
+  console.log(isDBT, block.content?.sql && props.languageId && visible)
 
   return el
     ? createPortal(
@@ -255,12 +258,12 @@ function TransclusionContentWidget(props: {
               >
                 {title}
               </h4>
-              {block.content?.sql && props.languageId && visible ? (
-                <div
-                  className={css`
-                    height: 300px;
-                  `}
-                >
+              <div
+                className={css`
+                  height: 300px;
+                `}
+              >
+                {block.content?.sql && props.languageId && visible ? (
                   <SQLViewer
                     blockId={block.id}
                     languageId={props.languageId}
@@ -270,8 +273,34 @@ function TransclusionContentWidget(props: {
                       padding: 0 15px;
                     `}
                   />
-                </div>
-              ) : null}
+                ) : isDBT ? (
+                  <MonacoEditor
+                    language="yaml"
+                    value={YAML.stringify(omit(block.content, 'title'))}
+                    options={{
+                      folding: false,
+                      wordWrap: 'on',
+                      contextmenu: false,
+                      scrollbar: { verticalScrollbarSize: 0, horizontalSliderSize: 0 },
+                      minimap: { enabled: false },
+                      lineNumbers: 'off',
+                      renderLineHighlight: 'none',
+                      glyphMargin: false,
+                      lineNumbersMinChars: 0,
+                      lineDecorationsWidth: 0,
+                      padding: { top: 10, bottom: 10 },
+                      lineHeight: 18,
+                      fontSize: 12,
+                      readOnly: true,
+                      scrollBeyondLastLine: false,
+                      scrollBeyondLastColumn: 0
+                    }}
+                    wrapperClassName={css`
+                      padding: 0 15px;
+                    `}
+                  />
+                ) : null}
+              </div>
               <div
                 className={css`
                   border-top: 1px solid ${ThemingVariables.colors.gray[1]};
