e26c5b65ff6ebce032d6d012fd1242d47040e539
Ray Zhang <m@raycursive.com>
2021-08-13 16:18:25 +0800

    correct the name of metabase, add links for v11n block

--- packages/api/src/core/block/visualization.ts
+++ packages/api/src/core/block/visualization.ts
@@ -1,10 +1,33 @@
 import { Block } from '.'
 import { BlockType } from '../../types/block'
+import { LinkType } from '../../types/link'
+import { Link } from '../link'
 
+type VisualizationBlockContent = {
+  dataAssetId: string
+  visualization?: Record<string, unknown>
+}
 export class VisualizationBlock extends Block {
   static type = BlockType.VISUALIZATION
 
   getType(): BlockType {
     return VisualizationBlock.type
+  }
+
+  private getContent(): VisualizationBlockContent {
+    return (this.content as VisualizationBlockContent) ?? {}
+  }
+
+  getLinksFromContent(): Link[] {
+    if (!this.alive) {
+      return []
+    }
+    const { dataAssetId } = this.getContent()
+    return [
+      {
+        blockId: dataAssetId,
+        type: LinkType.BLOCK,
+      },
+    ]
   }
 }
