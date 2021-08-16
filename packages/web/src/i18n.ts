6323cb48a58700f3be7556a99ca4990ef60cf3b5
cosformula <cosformula@gmail.com>
2021-08-16 20:17:32 +0800

    feat: locales

--- packages/web/src/i18n.ts
+++ packages/web/src/i18n.ts
@@ -2,17 +2,20 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
-
+import { resources } from './locales'
 export const languages: string[] = ['en-US', 'zh-CN']
 
+console.log(resources)
+
 i18n
-  .use(Backend)
+  // .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: '/locales/{{lng}}.json'
-    },
+    // backend: {
+    //   loadPath: '/locales/{{lng}}.json'
+    // },
+    resources: resources,
     fallbackLng: 'en-US',
     supportedLngs: languages,
     interpolation: {
---
+++ packages/web/src/locales/index.ts
@@ -0,0 +1,7 @@
+import enUS from './en-US.json'
+import zhCN from './zh-CN.json'
+
+export const resources = {
+  'en-USE': { translation: enUS },
+  'zh-CN': { translation: zhCN }
+}
