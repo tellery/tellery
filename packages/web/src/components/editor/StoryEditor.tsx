5e8ce68fd0e7f07e61589594a73901f62fb4f1ae
cosformula <cosformula@gmail.com>
2021-08-19 13:10:44 +0800

    fix: enter cursor

--- packages/web/src/components/editor/StoryEditor.tsx
+++ packages/web/src/components/editor/StoryEditor.tsx
@@ -946,7 +946,7 @@ const _StoryEditor: React.FC<{
                     ]
                   })
                 })
-                setSelectionAtBlockStart(newBlock)
+                setSelectionAtBlockStart(block)
               }
             }
           }
