5138e102486fe8b546c770da0f3c45139ed13bfd
cosformula <cosformula@gmail.com>
2021-08-30 15:11:25 +0800

    fix: selection error

--- packages/web/src/components/editor/helpers/tellerySelection.ts
+++ packages/web/src/components/editor/helpers/tellerySelection.ts
@@ -74,24 +74,24 @@ const findNodePositionAndOffset = (node: Node, offset: number) => {
   if (node === root) {
     return [offset, 0]
   }
-  const i = -1
+  let index = -1
   let tokenAccOffset = 0
 
   for (let i = 0; i < children.length; i++) {
     const child = children[i] as HTMLElement
 
-    const tokenIndex = child.dataset?.tokenIndex ? parseInt(child.dataset?.tokenIndex) : i + 1
-    if (tokenIndex !== i) {
-      i = tokenIndex
+    const tokenIndex = child.dataset?.tokenIndex ? parseInt(child.dataset?.tokenIndex) : index + 1
+    if (tokenIndex !== index) {
+      index = tokenIndex
       tokenAccOffset = 0
     }
 
     if (child === node) {
-      return [i, offset]
+      return [index, offset]
     }
     if (child?.contains?.(node)) {
       const accOffset = offset + tokenAccOffset + getNodeContentOffset(child, node)
-      return [i, accOffset]
+      return [index, accOffset]
     } else {
       tokenAccOffset += child.textContent?.length ?? 0
     }
@@ -100,7 +100,7 @@ const findNodePositionAndOffset = (node: Node, offset: number) => {
   if (offset === 0) {
     return [0, 0]
   } else {
-    return [i, 0]
+    return [index, 0]
   }
 }
 
