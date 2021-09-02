5b61e7cb08d67a6a6264eb14238261ad7fd03f83
Found Pan Tiger <renzholy@gmail.com>
2021-09-02 14:02:59 +0800

    Update SmartQueryConfig.tsx

--- packages/web/src/components/SmartQueryConfig.tsx
+++ packages/web/src/components/SmartQueryConfig.tsx
@@ -1,10 +1,11 @@
-import { IconCommonArrowRight } from '@app/assets/icons'
+import { IconCommonClose } from '@app/assets/icons'
 import { useBlock, useGetProfileSpec, useSnapshot } from '@app/hooks/api'
 import { ThemingVariables } from '@app/styles'
 import { Dimension, Editor } from '@app/types'
 import { css, cx } from '@emotion/css'
 import Tippy from '@tippyjs/react'
 import { lowerCase, uniq } from 'lodash'
+import { ReactNode } from 'react-router/node_modules/@types/react'
 import FormDropdown from './kit/FormDropdown'
 import { MenuItem } from './MenuItem'
 import { MenuWrapper } from './MenuWrapper'
@@ -37,16 +38,36 @@ export default function SmartQueryConfig(props: {
         className={css`
           width: 280px;
           border-right: 1px solid ${ThemingVariables.colors.gray[1]};
-          padding: 10px;
+          padding: 16px;
         `}
       >
-        {props.metricIds.map((metricId) =>
+        <h3
+          className={css`
+            margin: 0;
+            font-weight: 500;
+            font-size: 12px;
+            line-height: 15px;
+            color: ${ThemingVariables.colors.text[0]};
+          `}
+        >
+          Metrics
+        </h3>
+        {props.metricIds.map((metricId, index) =>
           queryBuilderBlock.content?.metrics?.[metricId] ? (
-            <MenuItem
+            <ConfigItem
               key={metricId}
-              title={queryBuilderBlock.content.metrics[metricId].name}
-              disabled={queryBuilderBlock.content.metrics[metricId].deprecated || props.metricIds.includes(metricId)}
-            />
+              onClick={() => {
+                props.onChange(
+                  props.metricIds.filter((_m, i) => i !== index),
+                  props.dimensions
+                )
+              }}
+              className={css`
+                margin-top: 8px;
+              `}
+            >
+              {queryBuilderBlock.content.metrics[metricId].name}
+            </ConfigItem>
           ) : null
         )}
         <FormDropdown
@@ -68,7 +89,7 @@ export default function SmartQueryConfig(props: {
           className={css`
             width: 100%;
             text-align: start;
-            margin-top: 10px;
+            margin-top: 8px;
           `}
         >
           Add metric
@@ -78,11 +99,35 @@ export default function SmartQueryConfig(props: {
         className={css`
           width: 280px;
           border-right: 1px solid ${ThemingVariables.colors.gray[1]};
-          padding: 10px;
+          padding: 16px;
         `}
       >
-        {props.dimensions.map((dimension) => (
-          <MenuItem key={dimension.name} title={dimension.name} />
+        <h3
+          className={css`
+            margin: 0;
+            font-weight: 500;
+            font-size: 12px;
+            line-height: 15px;
+            color: ${ThemingVariables.colors.text[0]};
+          `}
+        >
+          Dimensions
+        </h3>
+        {props.dimensions.map((dimension, index) => (
+          <ConfigItem
+            key={dimension.name}
+            onClick={() => {
+              props.onChange(
+                props.metricIds,
+                props.dimensions.filter((_d, i) => i !== index)
+              )
+            }}
+            className={css`
+              margin-top: 8px;
+            `}
+          >
+            {dimension.name}
+          </ConfigItem>
         ))}
         <FormDropdown
           menu={({ onClick }) => (
@@ -146,12 +191,45 @@ export default function SmartQueryConfig(props: {
           className={css`
             width: 100%;
             text-align: start;
-            margin-top: 10px;
+            margin-top: 8px;
           `}
         >
           Add dimension
         </FormDropdown>
       </div>
+    </div>
+  )
+}
+
+function ConfigItem(props: { children: ReactNode; onClick(): void; className?: string }) {
+  return (
+    <div
+      className={cx(
+        css`
+          height: 36px;
+          font-size: 14px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 0 4px 0 16px;
+          & > svg {
+            opacity: 0;
+          }
+          &:hover > svg {
+            opacity: 1;
+          }
+        `,
+        props.className
+      )}
+    >
+      {props.children}
+      <IconCommonClose
+        color={ThemingVariables.colors.gray[0]}
+        onClick={props.onClick}
+        className={css`
+          cursor: pointer;
+        `}
+      />
     </div>
   )
 }
