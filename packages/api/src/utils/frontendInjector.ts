b99cf7f6f41f2f39b8797c7278c18ff22a3c94e9
Ray Zhang <m@raycursive.com>
2021-09-02 16:48:36 +0800

    feat: add customized frontend env var and bump version in default config

--- packages/api/src/utils/frontendInjector.ts
+++ packages/api/src/utils/frontendInjector.ts
@@ -3,7 +3,17 @@ import path from 'path'
 import fs from 'fs'
 import { parse } from 'node-html-parser'
 
-export function injectFrontendEnv(frontendAssetPath: string) {
+export function loadFrontendEnvConfig(): Record<string, string> {
+  const fromConfigs = config.get<Record<string, string>>('frontendConfig')
+  const env = Object.fromEntries(
+    Object.entries(process.env)
+      .filter(([k, v]) => k.startsWith('FRONTEND_') && v)
+      .map(([k, v]) => [k.replace('FRONTEND_', ''), v]),
+  ) as Record<string, string>
+  return { ...env, ...fromConfigs }
+}
+
+export function injectFrontendEnv(frontendAssetPath: string): void {
   const indexPath = path.join(frontendAssetPath, 'index.html')
   if (!fs.existsSync(indexPath)) {
     console.warn('frontend asset not found')
@@ -12,6 +22,6 @@ export function injectFrontendEnv(frontendAssetPath: string) {
   const indexFileContent = fs.readFileSync(indexPath)
   const html = parse(indexFileContent.toString())
   const script = html.querySelector('#telleryBootstrap')
-  script.set_content(JSON.stringify(config.get<unknown>('frontendConfig')))
+  script.set_content(JSON.stringify(loadFrontendEnvConfig()))
   fs.writeFileSync(indexPath, html.toString())
 }
