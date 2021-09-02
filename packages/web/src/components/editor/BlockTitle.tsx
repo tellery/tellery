76397c2edc6191220ba19616f5764a018983f314
cosformula <cosformula@gmail.com>
2021-09-02 19:26:27 +0800

    fix: block title suspense

--- packages/web/src/components/editor/BlockTitle.tsx
+++ packages/web/src/components/editor/BlockTitle.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import type { Editor } from '@app/types'
 import { useBlockTitleToText } from './hooks/useBlockTitleText'
 
-export const BlockTitle: React.FC<{ block: Editor.BaseBlock }> = ({ block }) => {
+const BlockTitleContent: React.FC<{ block: Editor.BaseBlock }> = ({ block }) => {
   const text = useBlockTitleToText(block)
 
   return <>{text}</>
+}
+
+export const BlockTitle: React.FC<{ block: Editor.BaseBlock }> = ({ block }) => {
+  return (
+    <React.Suspense fallback={<></>}>
+      <BlockTitleContent block={block}></BlockTitleContent>
+    </React.Suspense>
+  )
 }
