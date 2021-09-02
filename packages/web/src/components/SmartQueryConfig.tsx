f4732db6cd79fbac949adf8ecbaf673267294d3d
Found Pan Tiger <renzholy@gmail.com>
2021-09-02 14:54:51 +0800

    Update SmartQueryConfig.tsx

--- packages/web/src/components/SmartQueryConfig.tsx
+++ packages/web/src/components/SmartQueryConfig.tsx
@@ -1,11 +1,11 @@
-import { IconCommonClose } from '@app/assets/icons'
+import { IconCommonAdd, IconCommonClose } from '@app/assets/icons'
 import { useBlock, useGetProfileSpec, useSnapshot } from '@app/hooks/api'
 import { ThemingVariables } from '@app/styles'
 import { Dimension, Editor } from '@app/types'
 import { css, cx } from '@emotion/css'
 import Tippy from '@tippyjs/react'
 import { lowerCase, uniq, uniqBy } from 'lodash'
-import { ReactNode } from 'react-router/node_modules/@types/react'
+import { ReactNode, useState } from 'react'
 import FormDropdown from './kit/FormDropdown'
 import { MenuItem } from './MenuItem'
 import { MenuWrapper } from './MenuWrapper'
@@ -20,6 +20,8 @@ export default function SmartQueryConfig(props: {
   const { data: queryBuilderBlock } = useBlock<Editor.QueryBuilder>(props.queryBuilderId)
   const snapshot = useSnapshot(queryBuilderBlock?.content?.snapshotId)
   const { data: spec } = useGetProfileSpec()
+  const [metricVisible, setMetricVisible] = useState(false)
+  const [dimensionVisible, setDimensionVisible] = useState(false)
 
   if (!queryBuilderBlock || !snapshot) {
     return null
@@ -41,17 +43,69 @@ export default function SmartQueryConfig(props: {
           padding: 16px;
         `}
       >
-        <h3
+        <div
           className={css`
-            margin: 0;
-            font-weight: 500;
-            font-size: 12px;
-            line-height: 15px;
-            color: ${ThemingVariables.colors.text[0]};
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
           `}
         >
-          Metrics
-        </h3>
+          <h3
+            className={css`
+              margin: 0;
+              font-weight: 500;
+              font-size: 12px;
+              line-height: 15px;
+              color: ${ThemingVariables.colors.text[0]};
+            `}
+          >
+            Metrics
+          </h3>
+          <Tippy
+            visible={metricVisible}
+            onClickOutside={() => {
+              setMetricVisible(false)
+            }}
+            interactive={true}
+            placement="left-start"
+            theme="tellery"
+            arrow={false}
+            offset={[0, 0]}
+            trigger="click"
+            content={
+              <MenuWrapper>
+                {Object.entries(queryBuilderBlock.content?.metrics || {}).map(([metricId, metric]) => (
+                  <MenuItem
+                    key={metricId}
+                    title={metric.name}
+                    disabled={metric.deprecated || props.metricIds.includes(metricId)}
+                    onClick={() => {
+                      props.onChange(uniq([...props.metricIds, metricId]), props.dimensions)
+                      setMetricVisible(false)
+                    }}
+                  />
+                ))}
+              </MenuWrapper>
+            }
+            className={css`
+              width: 100%;
+              text-align: start;
+              margin-top: 8px;
+            `}
+          >
+            <div>
+              <IconCommonAdd
+                color={ThemingVariables.colors.text[0]}
+                onClick={() => {
+                  setMetricVisible((old) => !old)
+                }}
+                className={css`
+                  cursor: pointer;
+                `}
+              />
+            </div>
+          </Tippy>
+        </div>
         {props.metricIds.map((metricId, index) =>
           queryBuilderBlock.content?.metrics?.[metricId] ? (
             <ConfigItem
@@ -70,30 +124,6 @@ export default function SmartQueryConfig(props: {
             </ConfigItem>
           ) : null
         )}
-        <FormDropdown
-          menu={({ onClick }) => (
-            <MenuWrapper>
-              {Object.entries(queryBuilderBlock.content?.metrics || {}).map(([metricId, metric]) => (
-                <MenuItem
-                  key={metricId}
-                  title={metric.name}
-                  disabled={metric.deprecated || props.metricIds.includes(metricId)}
-                  onClick={() => {
-                    props.onChange(uniq([...props.metricIds, metricId]), props.dimensions)
-                    onClick()
-                  }}
-                />
-              ))}
-            </MenuWrapper>
-          )}
-          className={css`
-            width: 100%;
-            text-align: start;
-            margin-top: 8px;
-          `}
-        >
-          Add metric
-        </FormDropdown>
       </div>
       <div
         className={css`
@@ -102,17 +132,137 @@ export default function SmartQueryConfig(props: {
           padding: 16px;
         `}
       >
-        <h3
+        <div
           className={css`
-            margin: 0;
-            font-weight: 500;
-            font-size: 12px;
-            line-height: 15px;
-            color: ${ThemingVariables.colors.text[0]};
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
           `}
         >
-          Dimensions
-        </h3>
+          <h3
+            className={css`
+              margin: 0;
+              font-weight: 500;
+              font-size: 12px;
+              line-height: 15px;
+              color: ${ThemingVariables.colors.text[0]};
+            `}
+          >
+            Dimensions
+          </h3>
+          <Tippy
+            visible={dimensionVisible}
+            onClickOutside={() => {
+              setDimensionVisible(false)
+            }}
+            interactive={true}
+            placement="left-start"
+            theme="tellery"
+            arrow={false}
+            offset={[0, 0]}
+            trigger="click"
+            content={
+              <MenuWrapper>
+                {snapshot.data.fields.map((field, index) =>
+                  field.sqlType &&
+                  spec?.queryBuilderSpec.bucketization[field.sqlType] &&
+                  Object.keys(spec.queryBuilderSpec.bucketization[field.sqlType]).length ? (
+                    <Tippy
+                      theme="tellery"
+                      placement="right-start"
+                      arrow={false}
+                      interactive={true}
+                      offset={[-12, 10]}
+                      content={
+                        <MenuWrapper>
+                          {Object.keys(spec.queryBuilderSpec.bucketization[field.sqlType]).map((func) => (
+                            <MenuItem
+                              key={func}
+                              title={func}
+                              disabled={
+                                !!props.dimensions.find(
+                                  (dimension) =>
+                                    dimension.fieldName === field.name &&
+                                    dimension.fieldType === field.sqlType &&
+                                    dimension.func === func
+                                )
+                              }
+                              onClick={() => {
+                                props.onChange(
+                                  props.metricIds,
+                                  uniqBy(
+                                    [
+                                      ...props.dimensions,
+                                      {
+                                        name: `${field.name} ${lowerCase(func)}`,
+                                        fieldName: field.name,
+                                        fieldType: field.sqlType!,
+                                        func
+                                      }
+                                    ],
+                                    (dimension) => `${dimension.name}${dimension.fieldType}${dimension.func}`
+                                  )
+                                )
+                                setDimensionVisible(false)
+                              }}
+                            />
+                          ))}
+                        </MenuWrapper>
+                      }
+                    >
+                      <MenuItem key={field.name + index} title={field.name} side={`${field.sqlType} >`} />
+                    </Tippy>
+                  ) : (
+                    <MenuItem
+                      key={field.name + index}
+                      title={field.name}
+                      side={field.sqlType}
+                      disabled={
+                        !!props.dimensions.find(
+                          (dimension) => dimension.fieldName === field.name && dimension.fieldType === field.sqlType
+                        )
+                      }
+                      onClick={() => {
+                        props.onChange(
+                          props.metricIds,
+                          uniqBy(
+                            [
+                              ...props.dimensions,
+                              {
+                                name: field.name,
+                                fieldName: field.name,
+                                fieldType: field.sqlType!
+                              }
+                            ],
+                            (dimension) => `${dimension.name}${dimension.fieldType}${dimension.func}`
+                          )
+                        )
+                        setDimensionVisible(false)
+                      }}
+                    />
+                  )
+                )}
+              </MenuWrapper>
+            }
+            className={css`
+              width: 100%;
+              text-align: start;
+              margin-top: 8px;
+            `}
+          >
+            <div>
+              <IconCommonAdd
+                color={ThemingVariables.colors.text[0]}
+                onClick={() => {
+                  setDimensionVisible((old) => !old)
+                }}
+                className={css`
+                  cursor: pointer;
+                `}
+              />
+            </div>
+          </Tippy>
+        </div>
         {props.dimensions.map((dimension, index) => (
           <ConfigItem
             key={dimension.name + index}
@@ -129,98 +279,6 @@ export default function SmartQueryConfig(props: {
             {dimension.name}
           </ConfigItem>
         ))}
-        <FormDropdown
-          menu={({ onClick }) => (
-            <MenuWrapper>
-              {snapshot.data.fields.map((field, index) =>
-                field.sqlType &&
-                spec?.queryBuilderSpec.bucketization[field.sqlType] &&
-                Object.keys(spec.queryBuilderSpec.bucketization[field.sqlType]).length ? (
-                  <Tippy
-                    theme="tellery"
-                    placement="right-start"
-                    arrow={false}
-                    interactive={true}
-                    offset={[-12, 10]}
-                    content={
-                      <MenuWrapper>
-                        {Object.keys(spec.queryBuilderSpec.bucketization[field.sqlType]).map((func) => (
-                          <MenuItem
-                            key={func}
-                            title={func}
-                            disabled={
-                              !!props.dimensions.find(
-                                (dimension) =>
-                                  dimension.fieldName === field.name &&
-                                  dimension.fieldType === field.sqlType &&
-                                  dimension.func === func
-                              )
-                            }
-                            onClick={() => {
-                              props.onChange(
-                                props.metricIds,
-                                uniqBy(
-                                  [
-                                    ...props.dimensions,
-                                    {
-                                      name: `${field.name} ${lowerCase(func)}`,
-                                      fieldName: field.name,
-                                      fieldType: field.sqlType!,
-                                      func
-                                    }
-                                  ],
-                                  (dimension) => `${dimension.name}${dimension.fieldType}${dimension.func}`
-                                )
-                              )
-                              onClick()
-                            }}
-                          />
-                        ))}
-                      </MenuWrapper>
-                    }
-                  >
-                    <MenuItem key={field.name + index} title={field.name} side={`${field.sqlType} >`} />
-                  </Tippy>
-                ) : (
-                  <MenuItem
-                    key={field.name + index}
-                    title={field.name}
-                    side={field.sqlType}
-                    disabled={
-                      !!props.dimensions.find(
-                        (dimension) => dimension.fieldName === field.name && dimension.fieldType === field.sqlType
-                      )
-                    }
-                    onClick={() => {
-                      props.onChange(
-                        props.metricIds,
-                        uniqBy(
-                          [
-                            ...props.dimensions,
-                            {
-                              name: field.name,
-                              fieldName: field.name,
-                              fieldType: field.sqlType!
-                            }
-                          ],
-                          (dimension) => `${dimension.name}${dimension.fieldType}${dimension.func}`
-                        )
-                      )
-                      onClick()
-                    }}
-                  />
-                )
-              )}
-            </MenuWrapper>
-          )}
-          className={css`
-            width: 100%;
-            text-align: start;
-            margin-top: 8px;
-          `}
-        >
-          Add dimension
-        </FormDropdown>
       </div>
     </div>
   )
@@ -232,7 +290,8 @@ function ConfigItem(props: { children: ReactNode; onClick(): void; className?: s
       className={cx(
         css`
           height: 36px;
-          font-size: 14px;
+          font-size: 12px;
+          color: ${ThemingVariables.colors.text[0]};
           display: flex;
           align-items: center;
           justify-content: space-between;
