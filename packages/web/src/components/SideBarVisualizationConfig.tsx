48b4019fa7c09b3370539987796fd200c98ddfa7
Found Pan Tiger <renzholy@gmail.com>
2021-09-09 15:58:45 +0800

    Update SideBarVisualizationConfig.tsx

--- packages/web/src/components/SideBarVisualizationConfig.tsx
+++ packages/web/src/components/SideBarVisualizationConfig.tsx
@@ -18,7 +18,7 @@ import produce from 'immer'
 import { WritableDraft } from 'immer/dist/internal'
 import { useCallback, useEffect, useState } from 'react'
 import IconButton from './kit/IconButton'
-import { charts } from './v11n/charts'
+import { charts, useChart } from './v11n/charts'
 import { Config, Type } from './v11n/types'
 
 const icons = {
@@ -34,10 +34,11 @@ const icons = {
 
 export default function SideBarVisualizationConfig<T extends Type = Type>(props: { storyId: string; blockId: string }) {
   const block = useBlockSuspense<Editor.VisualizationBlock>(props.blockId)
+  const config = block?.content?.visualization
+  const chart = useChart(config?.type || Type.TABLE)
   const { data: queryBlock } = useBlock<Editor.QueryBlock>(block.content?.queryId!)
   const snapshot = useSnapshot(queryBlock?.content?.snapshotId)
   const commit = useCommit()
-  const config = block?.content?.visualization
   const [cache, setCache] = useState<{ [T in Type]?: Config<T> }>({})
   useEffect(() => {
     setCache({})
@@ -53,42 +54,76 @@ export default function SideBarVisualizationConfig<T extends Type = Type>(props:
     },
     [block, commit, props.storyId]
   )
+  const handleConfigChange = useCallback(
+    (
+      key1: keyof Config<Type>,
+      value1: Config<Type>[keyof Config<Type>],
+      key2: keyof Config<Type>,
+      value2: Config<Type>[keyof Config<Type>],
+      key3: keyof Config<Type>,
+      value3: Config<Type>[keyof Config<Type>]
+    ) => {
+      setBlock((draft) => {
+        if (draft.content?.visualization) {
+          if (key1) {
+            draft.content.visualization[key1] = value1
+          }
+          if (key2) {
+            draft.content.visualization[key2] = value2
+          }
+          if (key3) {
+            draft.content.visualization[key3] = value3
+          }
+        }
+      })
+    },
+    [setBlock]
+  )
 
   return (
-    <div
-      className={css`
-        padding: 14px 8px;
-      `}
-    >
-      {Object.values(Type).map((t) => (
-        <IconButton
-          key={t}
-          icon={icons[t]}
-          color={t === config?.type ? ThemingVariables.colors.primary[1] : ThemingVariables.colors.gray[0]}
-          className={css`
-            margin: 2px;
-            border-radius: 8px;
-            height: 32px;
-            width: 32px;
-            background: ${t === config?.type ? ThemingVariables.colors.primary[4] : undefined};
-            display: inline-flex;
-            align-items: center;
-            justify-content: center;
-          `}
-          onClick={() => {
-            if (t === config?.type) {
-              return
-            }
-            setBlock((draft) => {
-              if (draft?.content) {
-                draft.content.visualization = snapshot?.data
-                  ? (charts[t].initializeConfig(snapshot.data, cache) as Config<T>)
-                  : undefined
+    <>
+      <div
+        className={css`
+          padding: 14px 8px;
+        `}
+      >
+        {Object.values(Type).map((t) => (
+          <IconButton
+            key={t}
+            icon={icons[t]}
+            color={t === config?.type ? ThemingVariables.colors.primary[1] : ThemingVariables.colors.gray[0]}
+            className={css`
+              margin: 2px;
+              border-radius: 8px;
+              height: 32px;
+              width: 32px;
+              background: ${t === config?.type ? ThemingVariables.colors.primary[4] : undefined};
+              display: inline-flex;
+              align-items: center;
+              justify-content: center;
+            `}
+            onClick={() => {
+              if (t === config?.type) {
+                return
               }
-            })
-          }}
+              setBlock((draft) => {
+                if (draft?.content) {
+                  draft.content.visualization = snapshot?.data
+                    ? (charts[t].initializeConfig(snapshot.data, cache) as Config<T>)
+                    : undefined
+                }
+              })
+            }}
+          />
+        ))}
+      </div>
+      {config && chart && chart.type === config.type && snapshot?.data ? (
+        <chart.Configuration
+          data={snapshot.data}
+          config={config as never}
+          onConfigChange={handleConfigChange as never}
         />
-      ))}
-    </div>
+      ) : null}
+    </>
   )
 }
