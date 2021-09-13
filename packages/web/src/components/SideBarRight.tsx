ae0b7111f59d1364dd65683924ffcb67791f3c52
Found Pan Tiger <renzholy@gmail.com>
2021-09-13 14:09:55 +0800

    fix: disable visualization tab if not visualization block

--- packages/web/src/components/SideBarRight.tsx
+++ packages/web/src/components/SideBarRight.tsx
@@ -1,12 +1,14 @@
 import { ThemingVariables } from '@app/styles'
 import { css } from '@emotion/css'
 import { Tab, TabList, TabPanel, useTabState } from 'reakit/Tab'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { SideBarDataAssets } from './SideBarDataAssets'
 import { useSideBarQuestionEditorState } from '../hooks/useSideBarQuestionEditor'
 import SideBarVisualizationConfig from './SideBarVisualizationConfig'
 import { SideBarTabHeader, StyledTabPanel } from './v11n/components/Tab'
+import { isVisualizationBlock } from './editor/Blocks/utils'
+import { useBlock } from '@app/hooks/api'
 
 export const SideBarRight: React.FC<{ storyId: string }> = ({ storyId }) => {
   const { t } = useTranslation()
@@ -14,7 +16,10 @@ export const SideBarRight: React.FC<{ storyId: string }> = ({ storyId }) => {
   const currentBlockId = sideBarEditorState?.blockId ?? storyId
   const tab = useTabState()
   const { setSelectedId } = tab
-
+  const currentBlock = useBlock(currentBlockId)
+  const showVisualizationTab = useMemo(() => {
+    return currentBlock.data && isVisualizationBlock(currentBlock.data.type)
+  }, [currentBlock.data])
   useEffect(() => {
     setSelectedId(sideBarEditorState?.activeTab || 'Data Assets')
   }, [setSelectedId, sideBarEditorState])
@@ -41,7 +46,13 @@ export const SideBarRight: React.FC<{ storyId: string }> = ({ storyId }) => {
         <Tab as={SideBarTabHeader} {...tab} id="Data Assets" selected={tab.selectedId === 'Data Assets'}>
           {t`Data Assets`}
         </Tab>
-        <Tab as={SideBarTabHeader} {...tab} id="Visualization" selected={tab.selectedId === 'Visualization'}>
+        <Tab
+          as={SideBarTabHeader}
+          {...tab}
+          id="Visualization"
+          selected={tab.selectedId === 'Visualization'}
+          disabled={!showVisualizationTab}
+        >
           {t`Visualization`}
         </Tab>
       </TabList>
