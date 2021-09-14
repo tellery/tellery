141c1617adfaf83e0faafb935f3540f95785507d
Ray Zhang <m@raycursive.com>
2021-09-14 11:28:39 +0800

    chore(deps): upgrade

--- packages/connector/implementation/build.gradle.kts
+++ packages/connector/implementation/build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 
 dependencies {
     implementation(project(":interface"))
-    implementation(platform("com.google.cloud:libraries-bom:22.0.0"))
-    implementation("net.snowflake:snowflake-jdbc:3.13.7")
+    implementation(platform("com.google.cloud:libraries-bom:23.0.0"))
+    implementation("net.snowflake:snowflake-jdbc:3.13.8")
     runtimeOnly("org.postgresql:postgresql:42.2.23")
     runtimeOnly("com.amazon.redshift:redshift-jdbc42:2.1.0.1")
     implementation("com.google.cloud:google-cloud-bigquery")
