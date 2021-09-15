f5dc5f179ace07f5f00a731537ffacfd436e410f
Found Pan Tiger <renzholy@gmail.com>
2021-09-15 16:49:17 +0800

    Update Tab.tsx

--- packages/web/src/components/v11n/components/Tab.tsx
+++ packages/web/src/components/v11n/components/Tab.tsx
@@ -6,16 +6,16 @@ export const SideBarTabHeader = styled.button<{ selected: boolean }>`
   font-weight: 500;
   height: 40px;
   font-size: 12px;
-  color: ${ThemingVariables.colors.text[1]};
   background: transparent;
   border: none;
   margin-left: 16px;
   padding: 0;
   cursor: pointer;
-  color: ${(props) => (props.selected ? ThemingVariables.colors.text[0] : ThemingVariables.colors.text[2])};
+  color: ${(props) => (props.selected ? ThemingVariables.colors.text[0] : ThemingVariables.colors.text[1])};
 
   :disabled,
   [aria-disabled] {
+    color: ${ThemingVariables.colors.text[2]};
     pointer-events: auto !important;
     cursor: not-allowed !important;
   }
