6893a99b84a6a151d11546d7f8049e51fb3b2a7d
cosformula <cosformula@gmail.com>
2021-09-17 20:12:42 +0800

    feat: disable replay

--- packages/web/src/utils/openReplay.ts
+++ packages/web/src/utils/openReplay.ts
@@ -1,10 +1,10 @@
 import { env } from '../env'
 import Tracker from '@openreplay/tracker'
 
-export let tracker: Tracker | null = null
-if (env.ASAYERIO_PROJECTID) {
-  tracker = new Tracker({
-    projectKey: env.ASAYERIO_PROJECTID
-  })
-  tracker.start()
-}
+export const tracker: Tracker | null = null
+// if (env.ASAYERIO_PROJECTID) {
+//   tracker = new Tracker({
+//     projectKey: env.ASAYERIO_PROJECTID
+//   })
+//   tracker.start()
+// }
