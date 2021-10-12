cfeb5216efd3822f2bf95b2ad8dfdff765417031
Ray Zhang <m@raycursive.com>
2021-10-12 16:57:51 +0800

    add retrycount and non-fixed delay

--- packages/api/src/utils/transaction.ts
+++ packages/api/src/utils/transaction.ts
@@ -4,11 +4,11 @@ import Bluebird from 'bluebird'
 export async function withSerializableTransaction<T>(
   m: EntityManager,
   runInTransaction: (m: EntityManager) => Promise<T>,
-  retryCount = 3,
+  retryCount = 5,
 ): Promise<T> {
   return m.transaction('SERIALIZABLE', runInTransaction).catch(async (err) => {
     if (err instanceof QueryFailedError && err.driverError.code === '40001' && retryCount > 0) {
-      await Bluebird.delay(5)
+      await Bluebird.delay(Math.floor(Math.random() * 100))
       return withSerializableTransaction(m, runInTransaction, retryCount - 1)
     } else {
       throw err
