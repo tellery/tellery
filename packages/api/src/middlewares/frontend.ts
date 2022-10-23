af8a1d9e98f2406311eef2ff7b9927b5ee5b560a
cosformula <cosformula@gmail.com>
2022-10-23 15:21:20 +0800

    feat: inject frontend env middleware

--- packages/api/src/middlewares/frontend.ts
+++ packages/api/src/middlewares/frontend.ts
@@ -6,38 +6,39 @@ import proxy from 'koa-better-http-proxy'
 import compose from 'koa-compose'
 import { historyApiFallback } from 'koa2-connect-history-api-fallback'
 import { loadFrontendEnvConfig } from '../utils/frontendInjector'
+import koa from 'koa'
 
 const frontendAssestDir = config.get<string>('frontendConfig.assestsUrl')
 const frontendHost = config.get<string>('frontendConfig.host')
 const isWebUrl = frontendAssestDir.startsWith('http')
 const isHttps = frontendAssestDir.startsWith('https')
 const staticDirPath = path.join(__dirname, frontendAssestDir)
-const webUrl = isWebUrl ? new URL(frontendAssestDir) : null
+const webUrl = isWebUrl ? new URL('http://localhost:3000') : null
 const frontendEnv = loadFrontendEnvConfig()
 const envString = JSON.stringify(frontendEnv)
+
+const injectEnvMiddleware: koa.Middleware = async (ctx, next) => {
+  const contentType = ctx.response.headers['content-type']
+  if (contentType && typeof contentType === 'string' && contentType.startsWith('text/html')) {
+    const body = (ctx.body as Buffer).toString()
+    ctx.body = Buffer.from(
+      body.replace(
+        /(<script .*?\s* id="telleryBootstrap">\s*)(\{.*?\})(\s*<\/script>)/s,
+        (match, p1, p2, p3) => {
+          return `${p1}${envString}${p3}`
+        },
+      ),
+      'utf-8',
+    )
+  }
+  await next()
+}
 
 const staticMiddleware = isWebUrl
   ? proxy(webUrl?.host ?? '', {
       proxyReqOptDecorator(request) {
         request.headers.host = frontendHost
         return request
-      },
-      userResDecorator(proxyRes, proxyResData, ctx) {
-        if (ctx.path === '/index.html') {
-          return Buffer.from(
-            (proxyResData as Buffer)
-              .toString()
-              .replace(
-                /(<script .*?\s* id="telleryBootstrap">\s*)(\{.*?\})(\s*<\/script>)/s,
-                (match, p1, p2, p3) => {
-                  return `${p1}${envString}${p3}`
-                },
-              ),
-            'utf-8',
-          )
-        } else {
-          return proxyResData
-        }
       },
       proxyReqPathResolver: (ctx) => {
         return `${webUrl?.pathname === '/' ? '' : webUrl?.pathname}${ctx.path}`
@@ -51,4 +52,5 @@ export default compose([
   mount('/api/static', serve(path.join(staticDirPath, 'images'))),
   historyApiFallback({ index: '/index.html', whiteList: ['/api'] }),
   staticMiddleware,
+  injectEnvMiddleware,
 ])
